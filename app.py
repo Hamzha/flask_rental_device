846c462b1a190fa21b96369b99c2650c70913580
Hamzha <31346801+Hamzha@users.noreply.github.com>
2020-11-05 15:26:21 +0500

    init

---
+++ app.py
@@ -0,0 +1,558 @@
+from flask import Flask,request,jsonify,make_response
+from datetime import datetime
+import db
+from flasgger import Swagger
+
+app = Flask(__name__)
+
+template = {
+  "swagger": "2.0",
+  "info": {
+    "title": "My API",
+    "description": "API for my data",
+    "contact": {
+      "responsibleOrganization": "ME",
+      "responsibleDeveloper": "Me",
+      "email": "me@me.com",
+      "url": "www.me.com",
+    },
+    "termsOfService": "http://me.com/terms",
+    "version": "1"
+  },
+}
+
+swagger = Swagger(app, template=template)
+
+
+# Add Device
+@app.route('/addDevice', methods=['POST'])
+def addDevice():
+
+    """ Call to Add Device
+    ---
+    consumes:
+      - application/json
+    produces:
+      - application/json
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+            required:
+              - device_id
+              - device_name
+              - vendor
+              - created_at
+              - is_device_in_hub
+              - created_at_hub
+            properties:
+                device_id:
+                    type: number
+                    example: 1
+                device_name:
+                    type: string
+                    example: "tk259"
+                vendor:
+                    type: string
+                    description: "asasd"
+                    example: "Ishfaq"
+                created_at:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+                is_device_in_hub:
+                    type: boolean
+                    example: "True"
+                created_at_hub:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+    responses:
+        200:
+            description: "Device is insesrt successfully"
+            msg: string
+            code: number
+        409:
+            description: "Device with same device_id exists."
+            msg: string
+            code: number
+        400: 
+            description: "Database Connection error OR Bad Request."
+            msg: string
+            code: number
+    """
+
+    if request.is_json:
+        req = request.get_json()
+        result = db.addDevice(req)
+        
+        if result == 200:
+            response = {
+                'message': 'OK'
+            }
+        
+            json_response = make_response(jsonify(response))
+            return json_response,200
+        elif result == 409:
+            response = {
+                'message': 'Device with same device_id exists.'
+            }
+        
+            json_response = make_response(jsonify(response))
+            return json_response,409
+        elif result == 400:
+            response = {
+                'message': 'Database Connection error.'
+            }
+        
+            json_response = make_response(jsonify(response))
+            return json_response,400
+        
+    else:
+        response = {
+                'message': 'Bad Request.'
+            }
+        json_response = make_response(jsonify(response))
+        return json_response,400
+
+# Add User
+@app.route('/addUser', methods=['POST'])
+def addUser():
+
+    """ Call to Add User
+    ---
+    consumes:
+      - application/json
+    produces:
+      - application/json
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+            required:
+              - user_id
+              - user_name
+              - address
+              - dob
+              - created_at
+            properties:
+                user_id:
+                    type: number
+                    example: 1
+                user_name:
+                    type: string
+                    example: "user_1"
+                address:
+                    type: string
+                    exmaple: "kalma chowk"
+                created_at:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+                dob:
+                    type: string
+                    example: "29-03-1991"
+                created_at_hub:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+    responses:
+        200:
+            description: "User is insesrt successfully"
+            msg: string
+            code: number
+        409:
+            description: "User with same user_id exists."
+            msg: string
+            code: number
+        400: 
+            description: "Database Connection error OR Bad Request."
+            msg: string
+            code: number
+    """
+
+    if request.is_json:
+        req = request.get_json()
+        result = db.addUser(req)
+        
+        if result == 200:
+            response = {
+                'message': 'OK'
+            }
+        
+            json_response = make_response(jsonify(response))
+            return json_response,200
+        if result == 409:
+            response = {
+                'message': 'User with same user_id exists.'
+            }
+        
+            json_response = make_response(jsonify(response))
+            return json_response,409
+        elif result == 400:
+            response = {
+                'message': 'Database Connection error.'
+            }
+        
+            json_response = make_response(jsonify(response))
+            return json_response,400
+        
+    else:
+        response = {
+                'message': 'Bad Request.'
+            }
+        json_response = make_response(jsonify(response))
+        return json_response,400
+
+# Modify Device
+@app.route('/modifyDevice', methods=['POST'])
+def modifyDevice():
+
+    """ Call to Modify Device
+    ---
+    consumes:
+      - application/json
+    produces:
+      - application/json
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+            required:
+              - device_id
+              - device_name
+              - vendor
+              - created_at
+              - is_device_in_hub
+              - created_at_hub
+            properties:
+                device_id:
+                    type: number
+                    example: 1
+                device_name:
+                    type: string
+                    example: "tk259"
+                vendor:
+                    type: string
+                    description: "asasd"
+                    example: "Ishfaq"
+                created_at:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+                is_device_in_hub:
+                    type: boolean
+                    example: "True"
+                created_at_hub:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+    responses:
+        200:
+            description: "Device is modified successfully"
+            msg: string
+            code: number
+        400: 
+            description: "Please Provide all required fields or Database Connection error or Bad Request"
+            msg: string
+            code: number
+        409:
+            description: "Device with this device_id does not exits."
+            msg: string
+            code: number        
+    """
+
+    if request.is_json:
+        req = request.get_json()
+        
+        if 'device_id' not in req or 'device_name' not in req:
+            response = {
+                'message': 'Please provide all required field.'
+            }
+            json_response = make_response(jsonify(response))
+            return json_response,400
+
+        else: 
+            result = db.modifyDevice(req)
+        
+            if result == 200:
+                response = {
+                    'message': 'OK'
+                }
+                json_response = make_response(jsonify(response))
+                return json_response,200
+            elif result == 409:
+                response = {
+                    'message': 'Device with this device_id does not exits.'
+                } 
+                json_response = make_response(jsonify(response))
+                return json_response,409
+            elif result == 400:
+                response = {
+                    'message': 'Database Connection error.'
+                } 
+                json_response = make_response(jsonify(response))
+                return json_response,400
+            
+    else:
+        response = {
+                'message': 'Bad Request.'
+            }
+        json_response = make_response(jsonify(response))
+        return json_response,400
+
+# Modify User
+@app.route('/modifyUser', methods=['POST'])
+def modifyUser():
+
+    """ Call to Modify User
+    ---
+    consumes:
+      - application/json
+    produces:
+      - application/json
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+            required:
+              - user_id
+              - user_name
+              - address
+              - dob
+              - created_at
+            properties:
+                user_id:
+                    type: number
+                    example: 1
+                user_name:
+                    type: string
+                    example: "user_1"
+                address:
+                    type: string
+                    exmaple: "kalma chowk"
+                created_at:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+                dob:
+                    type: string
+                    example: "29-03-1991"
+                created_at_hub:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+    responses:
+        200:
+            description: "User is modified successfully"
+            msg: string
+            code: number
+        400: 
+            description: "Please Provide all required fields or Database Connection error or Bad Request"
+            msg: string
+            code: number
+        409:
+            description: "Device with this user_id does not exits."
+            msg: string
+            code: number 
+    """
+    if request.is_json:
+        req = request.get_json()
+        
+        if 'user_id' not in req or 'user_name' not in req:
+            response = {
+                'message': 'Please provide all required field.'
+            }
+            json_response = make_response(jsonify(response))
+            return json_response,400
+
+        else: 
+            result = db.modifyUser(req)
+        
+            if result == 200:
+                response = {
+                    'message': 'OK'
+                }
+                json_response = make_response(jsonify(response))
+                return json_response,200
+            
+            elif result == 409:
+                response = {
+                'message': 'Device with this user_id does not exits.'
+                } 
+                json_response = make_response(jsonify(response))
+                return json_response,409
+            elif result == 400:
+                response = {
+                'message': 'Database Connection error.'
+                } 
+                json_response = make_response(jsonify(response))
+                return json_response,400
+    else:
+        response = {
+                'message': 'Bad Request.'
+            }
+        json_response = make_response(jsonify(response))
+        return json_response,400
+
+# Rent Device
+@app.route('/rentDevice', methods=['POST'])
+def rentDevice():
+    
+    """ Call to Modify User
+    ---
+    consumes:
+      - application/json
+    produces:
+      - application/json
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+            required:
+              - device_id
+              - rented_at
+              - rented_to_uid
+              - rented_to_name
+              - transaction_id
+              - is_returned
+              - returned_at
+            properties:
+                device_id:
+                    type: number
+                    example: 1
+                rented_at:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+                rented_to_uid:
+                    type: number
+                    exmaple: 1
+                rented_to_name:
+                    type: string
+                    example: "User_123"
+                transaction_id:
+                    type: number
+                    example: "1"
+                is_returned:
+                    type: boolean
+                    example: "False"
+                returned_at:
+                    type: string
+                    example: "29-03-2013 15:59:02"
+    responses:
+        200:
+            description: "User is modified successfully"
+            msg: string
+            code: number
+        400: 
+            description: "Database Connection error or Bad Request"
+            msg: string
+            code: number
+        409:
+            description: "device_id or user_id does not exist."
+            msg: string
+            code: number 
+    """
+
+    if request.is_json:
+        req = request.get_json()
+        
+        result = db.rentDevice(req)
+        
+        if result == 200:
+            response = {
+                'message': 'OK'
+            }
+            json_response = make_response(jsonify(response))
+            return json_response,200
+        elif result == 409:
+            response = {
+                'message': 'device_id or user_id does not exist.'
+            }
+            json_response = make_response(jsonify(response))
+            return json_response,409
+        elif result == 400:
+                response = {
+                'message': 'Database Connection error.'
+                } 
+                json_response = make_response(jsonify(response))
+                return json_response,400
+    else:
+        response = {
+                'message': 'Bad Request.'
+            }
+        json_response = make_response(jsonify(response))
+        return json_response,400
+
+# Return Device
+@app.route('/returnDevice', methods=['POST'])
+def returnDevice():
+
+
+    """ Call to Modify User
+    ---
+    consumes:
+      - application/json
+    produces:
+      - application/json
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+            required:
+              - device_id
+            properties:
+                device_id:
+                    type: number
+                    example: 1
+    responses:
+        200:
+            description: "is_returned set to true."
+            msg: string
+            code: number
+        400: 
+            description: "Database Connection error or Bad Request"
+            msg: string
+            code: number
+        409:
+            description: "Device was not rented."
+            msg: string
+            code: number 
+    """
+
+    if request.is_json:
+        req = request.get_json()
+        print("asdasd")
+        result = db.returnDevice(req)
+        
+        if result == 200:
+            response = {
+                'message': 'OK'
+            }
+
+            json_response = make_response(jsonify(response))
+            return json_response,200
+        elif result == 409:
+            response = {
+                'message': 'Device was not rented.'
+            }
+
+            json_response = make_response(jsonify(response))
+            return json_response,409
+            
+        elif result == 400:
+            response = {
+                'message': 'Database Connection error.'
+            }
+            json_response = make_response(jsonify(response))
+            return json_response,400
+
+    else:
+        response = {
+                'message': 'Wrong Request.'
+            }
+        json_response = make_response(jsonify(response))
+        return json_response,400
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
---
+++ db.py
@@ -0,0 +1,236 @@
+import psycopg2
+from datetime import datetime
+#connect to DB
+def psotgresql():
+    try:
+        con = psycopg2.connect(
+            host = '127.0.0.1',
+            database = 'Flask',
+            user= 'postgres',
+            password = 'postgres'
+        )
+    except:
+        print("Not connecting to postgres")
+        return
+    
+    cur = con.cursor()
+
+    try:
+        cur.execute('CREATE TABLE IF NOT EXISTS "devices_meta" ("device_id" VARCHAR NOT NULL,"device_name" VARCHAR NOT NULL, "vendor" VARCHAR, "created_at" TIMESTAMP, "is_device_in_hub" BOOLEAN, "created_at_hub" TIMESTAMP, CONSTRAINT "devices_meta_pk" PRIMARY KEY ("device_id"));')
+    except:
+        print('devices_meta devices already exists!!!')
+    
+    try:
+        cur.execute('CREATE TABLE IF NOT EXISTS "users_meta" ("user_id" VARCHAR NOT NULL,"user_name" VARCHAR NOT NULL,"address" VARCHAR,"dob" DATE NOT NULL,"created_at" TIMESTAMP,CONSTRAINT "users_meta_pk" PRIMARY KEY ("user_id"));')
+    except:
+        print('userss_meta devices already exists!!!')
+    
+    try:
+        cur.execute('CREATE TABLE IF NOT EXISTS "device_rentals" ("device_id" VARCHAR NOT NULL,"rented_at" TIMESTAMP NOT NULL,"rented_to_uid" VARCHAR NOT NULL,"rented_to_name" VARCHAR NOT NULL,"transaction_id" SERIAL NOT NULL,"is_returned" BOOLEAN,"returned_at" TIMESTAMP,FOREIGN KEY (device_id) REFERENCES devices_meta (device_id),FOREIGN KEY (rented_to_uid) REFERENCES users_meta (user_id),CONSTRAINT "device_rentals_pk" PRIMARY KEY ("transaction_id"));')
+    except:
+        print('devices_rentals devices already exists!!!')
+    
+    cur.close()
+    con.commit()
+    con.close()
+
+# psotgresql()
+
+def connect_postgre():
+    try :
+        con = psycopg2.connect(
+            host = '127.0.0.1',
+            database = 'Flask',
+            user= 'postgres',
+            password = 'postgres'
+        )
+        return con
+    except :
+        return False
+    
+def addDevice(req):
+    psotgresql()
+    date_created_at = datetime.strptime(req['created_at'], "%d-%m-%Y %H:%M:%S")
+    date_created_at_hub = datetime.strptime(req['created_at_hub'], "%d-%m-%Y %H:%M:%S")
+
+    inset_query = "INSERT INTO devices_meta (device_id, device_name, vendor, created_at ,is_device_in_hub, created_at_hub) VALUES ("+ str(req["device_id"] )+",'" + req["device_name"] +"','"+ req["vendor"] +"','"+ str(date_created_at) +"',"+ req["is_device_in_hub"] +",'"+ str(date_created_at_hub) +"');"
+    
+    con = connect_postgre()
+    if con == False:
+        print('Not connected')
+        return 400
+
+    else:
+        try:
+            cur = con.cursor()
+            cur.execute(inset_query)
+            con.commit()
+            cur.close()
+            con.close()
+            return 200
+        
+        except:
+            cur.close()
+            con.close()
+            return 409
+
+def addUser(req):
+    psotgresql()
+    date_dob = datetime.strptime(req['dob'], "%d-%m-%Y")
+    date_created_at = datetime.strptime(req['created_at'], "%d-%m-%Y %H:%M:%S")
+
+    inset_query = "INSERT INTO users_meta (user_id, user_name, address, dob ,created_at) VALUES ("+ str(req["user_id"] )+",'" + req["user_name"] +"','"+ req["address"] +"','"+ str(date_dob) +"','"+ str(date_created_at) +"');"
+    
+    con = connect_postgre()
+    if con == False:
+        print('Not connected')
+        return 400
+
+    else:
+        try:
+            cur = con.cursor()
+            cur.execute(inset_query)
+            con.commit()
+            cur.close()
+            con.close()
+            return 200
+        except:
+            cur.close()
+            con.close()
+            return 409
+
+def modifyDevice(req):
+    psotgresql()
+    update_query = "UPDATE devices_meta SET device_name = '" + req["device_name"] +"'"
+    
+    if 'vendor' in req:
+        update_query = update_query+", vendor = '"+ req["vendor"]+"'"
+    if 'created_at' in req:
+        date_created_at = datetime.strptime(req['created_at'], "%d-%m-%Y %H:%M:%S")
+        update_query = update_query+", created_at = '" + str(date_created_at) +"'"
+    if 'is_device_in_hub' in req:
+        update_query = update_query+ ",is_device_in_hub = '"+req["is_device_in_hub"] +"'"
+    if 'created_at_hub' in req:
+        date_created_at_hub = datetime.strptime(req['created_at_hub'], "%d-%m-%Y %H:%M:%S")
+        update_query = update_query+", created_at_hub = '"+ str(date_created_at_hub) +"'"
+    
+    
+    update_query = update_query+" WHERE device_id = '" +str(req["device_id"])+"';"
+    select_query = "SELECT * FROM devices_meta WHERE device_id = '" +str(req["device_id"])+"';"
+    
+    con = connect_postgre()
+    if con == False:
+        print('Not connected')
+        return 400
+
+    else:
+        cur = con.cursor()
+        cur.execute(select_query)
+        if cur.rowcount == 1:
+            cur.execute(update_query)
+            con.commit()
+            cur.close()
+            con.close()
+            return 200
+        else:
+            cur.close()
+            con.close()
+            return 409
+
+def modifyUser(req):
+    psotgresql()
+    select_query = "SELECT * FROM users_meta WHERE user_id = '" +str(req["user_id"])+"';"
+    update_query = "UPDATE users_meta SET user_name = '"+ req['user_name'] +"'"
+    
+    if 'address' in req:
+        update_query= update_query+", address = '" +req['address']+ "'"
+    if 'dob' in req:
+        date_dob = datetime.strptime(req['dob'], "%d-%m-%Y")
+        update_query= update_query+", dob = '"+ str(date_dob) +"'"
+    if 'created_at' in req:
+        date_created_at = datetime.strptime(req['created_at'], "%d-%m-%Y %H:%M:%S")
+        update_query= update_query+",created_at = '"+str(date_created_at)+"'"
+
+    update_query= update_query+" WHERE user_id = '" +str(req["user_id"])+"';"
+    con = connect_postgre()
+    if con == False:
+        print('Not connected')
+        return 400
+
+    else:
+        cur = con.cursor()
+        cur.execute(select_query)
+        if cur.rowcount == 1:
+            cur.execute(update_query)
+            con.commit()
+            cur.close()
+            con.close()
+            return 200
+        else:
+            cur.close()
+            con.close()
+            return 409
+
+def rentDevice(req):
+    psotgresql()
+    date_rented_at = datetime.strptime(req['rented_at'], "%d-%m-%Y %H:%M:%S")
+    date_returned_at = datetime.strptime(req['returned_at'], "%d-%m-%Y %H:%M:%S")
+
+    select_query_user = "SELECT * FROM users_meta WHERE user_id = '" +str(req["rented_to_uid"])+"';"
+    select_query_device = "SELECT * FROM devices_meta WHERE device_id = '" +str(req["device_id"])+"';"
+    insert_query = "INSERT INTO device_rentals (device_id, rented_at, rented_to_uid, rented_to_name, transaction_id, is_returned, returned_at) VALUES ("+ str(req["device_id"] )+",'" + str(date_rented_at)+"','"+ str(req["rented_to_uid"]) +"','"+ str(req['rented_to_name']) +"','"+ str(req['transaction_id']) +"','"+ str(req['is_returned']) +"','"+ str(date_returned_at) +"');"
+
+    con = connect_postgre()
+
+    if con == False:
+        print('Not connected')
+        return 400
+    else:
+        cur = con.cursor()
+        cur.execute(select_query_user)
+        user_count = cur.rowcount
+        cur.execute(select_query_device)
+        device_count = cur.rowcount
+        if user_count == 1 and device_count == 1:
+            cur.execute(insert_query)
+            con.commit()
+            cur.close()
+            con.close()
+            return 200
+        else:
+            print(user_count, device_count)
+            cur.close()
+            con.close()
+            return 409
+        
+def returnDevice(req):
+    psotgresql()
+    select_query_device = "SELECT * FROM device_rentals WHERE is_returned = '" +str("false")+"' AND device_id='" +str(req["device_id"]) + "';"
+    con = connect_postgre()
+
+    if con == False:
+        print('Not connected')
+        return 400
+    else:
+        cur = con.cursor()
+        cur.execute(select_query_device)
+        user_count = cur.rowcount
+        if user_count == 0:
+            cur.close()
+            con.close()
+            return 409
+        else:
+            devices = cur.fetchall()
+            for value in devices:
+                if(value[5] == False):
+                    update_query_device = "UPDATE device_rentals SET is_returned = '" +str("true")+"' , device_id='" +str(req["device_id"]) + "' WHERE is_returned = '" +str("false")+"' AND device_id='" +str(req["device_id"]) + "';"
+                    cur.execute(update_query_device)
+                    con.commit()
+                    cur.close()
+                    con.close()
+                    return 200
+                else:
+                    cur.close()
+                    con.close()
+                    return 409
+                
